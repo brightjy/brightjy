801aa542954277e27b0bb638b18628bb6415dca6
brightjy <barkji0@naver.com>
2020-09-20 21:10:13 +0900

    Updates

---
+++ precache-manifest.016b0b4703fbbb6110568457da4f8209.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392411f058bab06dc47c08c6944b2a6f",
+    "url": "/brightjy/index.html"
+  },
+  {
+    "revision": "4b03ded1999e6547458f",
+    "url": "/brightjy/static/js/2.0390201f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brightjy/static/js/2.0390201f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e92e4e596905007239",
+    "url": "/brightjy/static/js/main.03036828.chunk.js"
+  },
+  {
+    "revision": "4db242821742c1038974",
+    "url": "/brightjy/static/js/runtime-main.bda80b50.js"
+  }
+]);
---
+++ static/js/main.03036828.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbrightjy=this.webpackJsonpbrightjy||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),l=n(2);var c=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"Hello!!"))};n.n(l).a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(c,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.03036828.chunk.js.map
