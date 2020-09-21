4243e3119e1dcf4f6f83ba383a30f9c283428e66
brightjy <barkji0@naver.com>
2020-09-21 22:51:10 +0900

    add info component

---
+++ src/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Info() {
+    return (
+        <h2>
+            <p><a href="https://github.com/brightjy">work for</a></p>
+            <p><a href="https://barkji0.blog.me">usually do</a></p>
+        </h2>
+    );
+}
+
+export default Info;
